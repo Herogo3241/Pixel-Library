b1e4033489e510227d729652283ef289653c6970
Goureesh Chandra <120293245+Herogo3241@users.noreply.github.com>
2025-01-11 19:37:03 +0530

    Update pixel.h

--- src/pixel.h
+++ src/pixel.h
@@ -22,14 +22,14 @@ extern int width, height, maxvalue;
 extern uint8_t **rChannel, **gChannel, **bChannel;
 
 
-void startCanvas(int screenWidth, int screenHeight);
-void setPixel(Vec2 pos, Color color);
-Color getPixel(Vec2 pos);
-void display();
-void animate(int frameNo);
-void saveVideo(const char* filename, int frameRate);
-void saveAsImage(const char* filename);
-void endCanvas();
+void startCanvas(int screenWidth, int screenHeight); // to generate the main window
+void setPixel(Vec2 pos, Color color); // setting pixel
+Color getPixel(Vec2 pos); // getting the color of the pixel
+void display(); // to display the window
+void animate(int frameNo); // for a storing each frame
+void saveVideo(const char* filename, int frameRate); // animate must be called before saving the window
+void saveAsImage(const char* filename); // call display before saving the image
+void endCanvas(); // to free the allocates memory and close the window
 
 
 
